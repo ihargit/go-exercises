3475c36da58f652a1b78500897617cf76d6d1515
Ihar Kharashun <ihar.kharashun@gmail.com>
2022-01-09 07:47:04 +0300

    fix: exercise-equivalent-binary-trees

--- aTourOfGo/exercise-equivalent-binary-trees.go
+++ aTourOfGo/exercise-equivalent-binary-trees.go
@@ -7,19 +7,18 @@ import (
 
 // Walk walks the tree t sending all values
 // from the tree to the channel ch.
-func Walk(t *tree.Tree, ch chan int, top bool) {
-	if t.Left != nil {
-		Walk(t.Left, ch, false)
-	}
-	if t.Value != 0 {
+func Walk(t *tree.Tree, ch chan int) {
+	var walk func (t *tree.Tree)
+	walk = func (t *tree.Tree) {
+		if t == nil {
+			return
+		}
+		walk(t.Left)
 		ch <- t.Value
+		walk(t.Right)
 	}
-	if t.Right != nil {
-		Walk(t.Right, ch, false)
-	}
-	if top {
-		close(ch)
-	}
+	walk(t)
+	close(ch)
 }
 
 // Same determines whether the trees
@@ -27,20 +26,27 @@ func Walk(t *tree.Tree, ch chan int, top bool) {
 func Same(t1, t2 *tree.Tree) bool {
 	ch1 := make(chan int)
 	ch2 := make(chan int)
-	go Walk(t1, ch1, true)
-	go Walk(t2, ch2, true)
-	for <-ch1 != <-ch2 {
-		return false
+	go Walk(t1, ch1)
+	go Walk(t2, ch2)
+	for {
+		x, ok1 := <-ch1
+		y, ok2 := <-ch2
+		if x != y || ok1 != ok2 {
+			return false
+		}
+		if !ok1 || !ok2 {
+			break
+		}
 	}
 	return true
 }
 
 func main() {
 	//ch := make(chan int)
-	//go Walk(tree.New(1), ch, true)
+	//go Walk(tree.New(1), ch)
 	//for i := range ch {
 	//	fmt.Println(i)
 	//}
-	isSame := Same(tree.New(1),tree.New(2))
+	isSame := Same(tree.New(1),tree.New(1))
 	fmt.Printf("The same: %v", isSame)
 }
